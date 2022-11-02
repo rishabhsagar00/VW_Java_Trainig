e02d22b96d772dce5c6f55eb431dd845f88d6a0c
rishabh sagar <76914330+rishabhsagar00@users.noreply.github.com>
2022-11-02 10:30:39 +0530

    Add files via upload

---
+++ collections/ArrayListEx.java
@@ -0,0 +1,45 @@
+package collections;
+ 
+import java.util.*;
+ 
+public class ArrayListEx {
+
+
+    public static void main(String[] args)
+    {
+
+        List<Integer> lst = new ArrayList<Integer>();
+
+
+
+        lst.add(80);  // new Integer(80) -- autoboxing - box the primitve into object type
+        lst.add(30);
+
+    //    lst.add("java");
+        lst.add(10);
+        lst.add(1);
+
+
+         System.out.println(lst);   //lst.toString();
+
+         System.out.println(lst.size());
+
+         int no = lst.get(0); //  new Integer(100).intvalue() -- auto-unboxing - unbox Integer object to its primitive value
+         System.out.println("value at index position 0:"+no);
+
+         System.out.println(lst); 
+         System.out.println(lst.size());
+
+          lst.remove(0);
+
+          System.out.println(lst);   
+          System.out.println(lst.size());
+
+          lst.set(0,130);
+          System.out.println(lst); 
+          System.out.println(lst.size());
+
+
+    }
+ 
+}
---
+++ collections/ArrayListEx_Name.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.awt.List;
+
+public class ArrayListEx_Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> lstt= new ArrayList<String>();
+		
+		lstt.add("A");
+		lstt.add("B");
+		lstt.add("C");
+		lstt.add("D");
+		lstt.add("E");
+		lstt.add("F");
+		
+		System.out.println(lstt);
+//		char ne= lstt.get(0);
+
+	}
+
+}
